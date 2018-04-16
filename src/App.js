93caffa1df1f19bff0317d3ca8bcfb5b2ed4fb79
Miguel Santamaria <msantamaria@H5746.ad.hugeinc.com>
2018-04-16 10:19:46 -0500

    Styling css file vs inline scoped

--- src/App.js
+++ src/App.js
@@ -33,11 +33,21 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Yo yo yo</h1>
         <p>Hello there</p>
-        <button onClick={() => this.switchNameHandler('Homero!!')}>Click me!</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Homero!!')}>Click me!</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}/>
